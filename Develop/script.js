b1f73842384809db93b41e0e58d5d23a8a308e5a
Sadiq Momin <sadiqmomin2009@gmail.com>
2022-05-10 07:50:28 -0500

    javascript addon

--- Develop/script.js
+++ Develop/script.js
@@ -14,3 +14,38 @@ function generatePassword() {
     else {
       alert("Your password will be " + pickLength + " characters long.");
     }
+    
+    pickUppercase = confirm("Do you want your password to include UPPERCASE?");
+    if (pickUppercase){
+      alert("Your new password will include UPPERCASE letters");
+    }
+    else {
+      alert("Your new password will NOT include UPPERCASE letters");
+    }
+
+  
+    pickLowercase = confirm("Do you want your password to include lowercase?");
+    if (pickLowercase){
+      var makeLowercase = alert("Your new password will include lowercase letters");
+    }
+    else {
+      alert("Your new password will NOT include lowercase letters");
+    }
+
+  
+    pickNumber = confirm("Do you want your password to include numbers?");
+    if (pickNumber){
+      alert("Your new password will include numbers");
+    }
+    else {
+      alert("Your new password will NOT include numbers");
+    }
+
+  
+    pickSpecial = confirm("Do you want your password to include special characters?");
+    if (pickSpecial){
+      alert("Your new password will include special characters");
+    }
+    else {
+      alert("Your new password will NOT include special characters");
+    }
