3ca570bc2b2ac1327012d4656114280e7d512023
Sadiq Momin <sadiqmomin2009@gmail.com>
2022-05-10 07:47:50 -0500

    javascript start

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,16 @@
 // Assignment code here
+function generatePassword() {
+  var lowercasePW = "abcdefghijklmnopqrstuvwxyz";
+  var uppercasePW = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbersPW = "0123456789";
+  var specialPW = "!@#$^&%*()+=-[]{}|:<>?,.";
+  var newPassword = "";
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    pickLength = prompt("How many characters would you like your new password to be? Pick between 8-128 characters");
+    if (pickLength < 8 || pickLength > 128 || isNaN(pickLength)){
+      prompt("Password value must be a NUMBER that is greater than 8 characters long and less than 128 characters!");
+    }
+    else {
+      alert("Your password will be " + pickLength + " characters long.");
+    }
